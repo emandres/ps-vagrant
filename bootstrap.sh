67626e72fe98ff3fef718d322c47992b55d1832e
Eric Andres <emandres@gmail.com>
2015-02-19 21:55:35 -0700

    Fixed rabbit to install manager

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ apt-get install -qy lxc-docker
 docker pull rabbitmq
 docker pull redis
 
-docker run --restart=always -d -p 5672:5672 -p 15672:15672 rabbitmq
+docker run --restart=always -d -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 docker run --restart=always -d -p 6379:6379 redis
